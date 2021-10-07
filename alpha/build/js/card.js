45f87aadb27ccc18e164f8d75905b561063ae0fc
Sylar Li <65619327+OwOsas@users.noreply.github.com>
2021-10-07 17:05:52 -0400

    Alpha Build

---
+++ alpha/build/js/card.js
@@ -0,0 +1,12 @@
+function card_bk_onclick(){
+    
+}
+
+var arrows = document.getElementsByClassName("arrow");
+
+for (var i = 0; i < arrows.length; i++){
+    arrows[i].addEventListener("click", function(){
+        
+
+    })
+}
